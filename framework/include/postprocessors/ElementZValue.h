11d5398b38d16cb8da4c68d31ea1d07f7f3bb13d
magnoxemo <walid.ahammed01@gmail.com>
2024-12-22 12:33:39 -0600

    working on z value indicator

---
+++ framework/include/postprocessors/ElementZValue.h
@@ -0,0 +1,34 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once 
+
+#include "ElementAverageValue.h"
+/**
+ * This postprocessor computes a volume integral of the specified variable.
+ *
+ * Note that specializations of this integral are possible by deriving from this
+ * class and overriding computeQpIntegral().
+ */
+class ElementZvalue : public ElementAverageValue
+{
+public:
+  static InputParameters validParams();
+
+  ElementZValue(const InputParameters & parameters);
+
+  virtual void initialize() override;
+  virtual void execute() override;
+  virtual Real getValue() const override;
+  virtual void finalize() override;
+  virtual void threadJoin(const UserObject & y) override;
+
+protected:
+  Real _volume;
+};
